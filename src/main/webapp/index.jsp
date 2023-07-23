1a33c791e7935a215d2bbe6e0247cc1afd799fbc
Ramen416 <blueberryapril16@gmail.com>
2023-07-23 21:57:31 +0900

    third

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -12,27 +12,57 @@
 <link href="https://fonts.googleapis.com/css2?family=Rakkas&family=Roboto&family=Zen+Maru+Gothic:wght@500&display=swap" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link href="https://fonts.googleapis.com/css2?family=Rakkas&family=Roboto&family=Zen+Antique&family=Zen+Maru+Gothic:wght@500&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link href="https://fonts.googleapis.com/css2?family=Changa:wght@500&family=Rakkas&family=Roboto&family=Zen+Antique&family=Zen+Maru+Gothic:wght@500&display=swap" rel="stylesheet">
 </head>
 <body style="overflow-x:hidden">
 	<div id="wrap">
 		
 		<header>
-			<strong class="logo"><span id="logo">My Portfolio</span></strong>
+			<strong class="logo"><span id="logo"><a href="/index.jsp">My Portfolio</a></span></strong>
 			<nav>
 				<ul>
-					<li><a href="#">自己紹介</a></li>
-					<li><a href="#">経歴</a></li>
-					<li><a href="#">プロジェクト</a></li>
-					<li><a href="#">そのた</a></li>
+					<li><a href="#" id="a1" onclick="clk1()">自己紹介</a></li>
+					<li><a href="#" onclick="clk2()">経歴</a></li>
+					<li><a href="#" onclick="clk3()">プロジェクト</a></li>
+					<li><a href="#" onclick="clk4()">そのた</a></li>
 				</ul>
 			</nav>
 		</header>
 		<section id="container">
-			<section id="menu1" class="content" onclick="call_box1()"></section>
-			<section id="menu2" class="content"></section>
-			<section id="menu3" class="content"></section>
-			<section id="menu4" class="content"></section>
-			<section id="menu1_box" class="box1" ></section>
+			<section id="menu1" class="content" onclick="call_box1()">
+				<div id="img1"></div>
+			</section>
+			<section id="menu2" class="content" onclick="call_box2()">
+				<div id="img2"></div>
+			</section>
+			<section id="menu3" class="content" onclick="call_box3()">
+				<div id="img3"></div>
+			</section>
+			<section id="menu4" class="content" onclick="call_box4()">
+				<div id="img4"></div>
+			</section>
+			<section id="menu1_box" class="box1">
+				<div id="close1" onclick="close1()"><strong>>></strong></div>
+				<div id="box_content1">
+					<img src="./images/untitled.png" id="picture"><br>
+					adssaddsdadad
+				</div>
+			</section>
+			<section id="menu2_box" class="box2">
+				<div id="close2" onclick="close2()">>></div>
+			</section>
+			<section id="menu3_box" class="box3">
+				<div id="close3" onclick="close3()"> << </div>
+			</section>
+			<section id="menu4_box" class="box4">
+				<div id="close4" onclick="close4()"><<</div>
+				<div id="box_content4">
+					E-MAIL : blueberryapril16@gmail.com <br>
+					TEL : (+82)10-3526-8957	<br>
+					GITHUB : https://github.com/Ramen416/ <br>
+				</div>
+			</section>
 		</section>
 		
 		<footer>
--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,13 +1,234 @@
 
 
 var box1 = document.getElementById('menu1_box');
+var box2 = document.getElementById('menu2_box');
+var box3 = document.getElementById('menu3_box');
+var box4 = document.getElementById('menu4_box');
+var menu1 = document.getElementById('menu1');
+var menu2 = document.getElementById('menu2');
+var menu3 = document.getElementById('menu3');
+var menu4 = document.getElementById('menu4');
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
+var img4 = document.getElementById('img4');
 
+function out1() {
+	img1.style.opacity="0.5";
+};	
 
+function out2() {
+	img2.style.opacity="0.5";
+};	
+
+function out3() {
+	img3.style.opacity="0.5";
+};	
+
+function out4() {
+	img4.style.opacity="0.5";
+};	
+
+function close1() {
+	img1.style.opacity="0.5";
+	img2.style.opacity="0.5";
+	img3.style.opacity="0.5";
+	img4.style.opacity="0.5";
+	box1.style.right="-100%";
+	box2.style.right="-100%";
+	box3.style.left="-100%";
+	box4.style.left="-100%";
+}
+
+function close2() {
+	menu1.style.left = "0%";
+	menu2.style.left = "25%";
+	img1.style.opacity="0.5";
+	img2.style.opacity="0.5";
+	img3.style.opacity="0.5";
+	img4.style.opacity="0.5";
+	box1.style.right="-100%";
+	box2.style.right="-100%";
+	box3.style.left="-100%";
+	box4.style.left="-100%";
+}
+
+function close3() {
+	menu4.style.left = "75%";
+	menu3.style.left = "50%";
+	img1.style.opacity="0.5";
+	img2.style.opacity="0.5";
+	img3.style.opacity="0.5";
+	img4.style.opacity="0.5";
+	box1.style.right="-100%";
+	box2.style.right="-100%";
+	box3.style.left="-100%";
+	box4.style.left="-100%";
+}
+
+function close4() {
+	img1.style.opacity="0.5";
+	img2.style.opacity="0.5";
+	img3.style.opacity="0.5";
+	img4.style.opacity="0.5";
+	box1.style.right="-100%";
+	box2.style.right="-100%";
+	box3.style.left="-100%";
+	box4.style.left="-100%";
+}
 
 
 function call_box1() {
 	box1.style.transition = "0.5s";
 	box1.style.right="0%";
+	box2.style.right="-100%";
+	box3.style.left="-100%";
+	box4.style.left="-100%";
+	img1.style.opacity="1";
 	
 	
+};
+
+function call_box2() {
+	img2.style.opacity="1";
+	box2.style.transition = "0.5s";
+	img3.style.opacity="0";
+	img4.style.opacity="0";
+	menu2.style.left = "0%";
+	menu2.style.transition="0.5s";
+	
+	box2.style.right="0%";
+	box1.style.right="-100%";
+	box3.style.left="-100%";
+	box4.style.left="-100%";
+	
+};
+
+function call_box3() {
+	img3.style.opacity="1";
+	box3.style.transition = "0.5s";
+	img1.style.opacity="0";
+	img2.style.opacity="0";
+	menu3.style.left = "75%";
+	menu3.style.transition="0.5s";
+	img4.style.opacity="0";
+
+	box3.style.left="0";
+	box1.style.right="-100%";
+	box2.style.right="-100%";
+	box4.style.left="-100%";
+};
+
+function call_box4() {
+	img4.style.opacity="1";
+	box4.style.transition = "0.5s";
+	img1.style.opacity="0";
+	img2.style.opacity="0";
+	img3.style.opacity="0";
+	box4.style.left="0";
+	box1.style.right="-100%";
+	box2.style.right="-100%";
+	box3.style.left="-100%";
+	
+	
+	
+	
+};
+
+function on1() {
+	if(img2.style.opacity="0.5") {
+		img1.style.opacity="1";
+	} else if (img2.style.opacity="1") {
+	img1.style.opacity="0";
+	} else if (img1.style.opacity="0.5") {
+	
+	}
+};
+
+function clk1() {
+	menu1.style.left="0%";
+	menu2.style.left="25%";
+	box1.style.transition = "0.5s";
+	box1.style.right="0%";
+	box2.style.right="-100%";
+	box3.style.left="-100%";
+	box4.style.left="-100%";
+	
+	img2.style.opacity="0";
+	img1.style.opacity="1";
+};
+
+function on2() {
+	if(img1.style.opacity="0.5") {
+		img2.style.opacity="1";
+	} else if (img1.style.opacity="1") {
+	img1.style.opacity="0";
+	} 
+
+	
+	
+	
+	
+};
+
+function clk2() {
+	img2.style.opacity="1";
+	box2.style.transition = "0.5s";
+	img3.style.opacity="0";
+	img4.style.opacity="0";
+	menu2.style.left = "0%";
+	menu1.style.left = "-300px";
+	menu2.style.transition="0.5s";
+	
+	img1.style.opacity="0";
+	
+	box2.style.right="0%";
+	box1.style.right="-100%";
+	box3.style.left="-100%";
+	box4.style.left="-100%";
+};
+
+function on3() {
+	img3.style.opacity="1";
+	menu3.style.zindex = "10";
+	
+	
+};
+
+function clk3() {
+	img3.style.opacity="1";
+	box3.style.transition = "0.5s";
+	img1.style.opacity="0";
+	img2.style.opacity="0";
+	menu3.style.left = "75%";
+	menu4.style.left="100%";
+	menu3.style.transition="0.5s";
+	img4.style.opacity="0";
+	
+	box3.style.left="0";
+	box1.style.right="-100%";
+	box2.style.right="-100%";
+	
+	box4.style.left="-100%";
+};
+
+function on4() {
+	img4.style.opacity="1";
+	menu3.style.zindex = "10";
+	
+	
+};
+
+function clk4() {
+	menu4.style.left="75%";
+	img4.style.opacity="1";
+	box4.style.transition = "0.5s";
+	img1.style.opacity="0";
+	img2.style.opacity="0";
+	img3.style.opacity="0";
+	box4.style.left="0";
+	box1.style.right="-100%";
+	box2.style.right="-100%";
+	box3.style.left="-100%";
+	img4.style.opacity="1";
 };
